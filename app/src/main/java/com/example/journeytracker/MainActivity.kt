d1838901dfa8714797166f16134de766ad689478
Kartavya Chauhan <kartavyachauhan2005@gmail.com>
2025-02-06 23:53:53 +0530

    Temporary commit before switching branches

--- app/src/main/java/com/example/journeytracker/MainActivity.kt
+++ app/src/main/java/com/example/journeytracker/MainActivity.kt
@@ -2,46 +2,14 @@ package com.example.journeytracker
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.journeytracker.ui.theme.JourneyTrackerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            JourneyTrackerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JourneyTrackerTheme {
-        Greeting("Android")
+        // Set the XML layout as the content view
+        setContentView(R.layout.activity_main)
     }
 }
